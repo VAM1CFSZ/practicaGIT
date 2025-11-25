385e2b3795452c93dc3e98b27ac80c5d25cd985e
VAM1CSFZ <vicabemar@alu.edu.gva.es>
2025-11-25 10:56:04 +0100

    Commit

--- ejercicioTelegrama/ejercicioTelegrama/Form1.Designer.cs
+++ ejercicioTelegrama/ejercicioTelegrama/Form1.Designer.cs
@@ -28,12 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            btnCalcularPrecio = new Button();
+            txtPrecio = new TextBox();
+            txtTelegrama = new TextBox();
+            chkUrgente = new CheckBox();
+            SuspendLayout();
+            // 
+            // btnCalcularPrecio
+            // 
+            btnCalcularPrecio.Location = new Point(245, 87);
+            btnCalcularPrecio.Name = "btnCalcularPrecio";
+            btnCalcularPrecio.Size = new Size(112, 34);
+            btnCalcularPrecio.TabIndex = 0;
+            btnCalcularPrecio.Text = "Calcular precio";
+            btnCalcularPrecio.UseVisualStyleBackColor = true;
+            btnCalcularPrecio.Click += btnCalcularPrecio_Click;
+            // 
+            // txtPrecio
+            // 
+            txtPrecio.Location = new Point(245, 162);
+            txtPrecio.Name = "txtPrecio";
+            txtPrecio.Size = new Size(150, 31);
+            txtPrecio.TabIndex = 1;
+            // 
+            // txtTelegrama
+            // 
+            txtTelegrama.Location = new Point(245, 235);
+            txtTelegrama.Name = "txtTelegrama";
+            txtTelegrama.Size = new Size(150, 31);
+            txtTelegrama.TabIndex = 2;
+            // 
+            // chkUrgente
+            // 
+            chkUrgente.AutoSize = true;
+            chkUrgente.Location = new Point(248, 316);
+            chkUrgente.Name = "chkUrgente";
+            chkUrgente.Size = new Size(101, 29);
+            chkUrgente.TabIndex = 3;
+            chkUrgente.Text = "Urgente";
+            chkUrgente.UseVisualStyleBackColor = true;
+            chkUrgente.CheckedChanged += chkUrgente_CheckedChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(chkUrgente);
+            Controls.Add(txtTelegrama);
+            Controls.Add(txtPrecio);
+            Controls.Add(btnCalcularPrecio);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnCalcularPrecio;
+        private TextBox txtPrecio;
+        private TextBox txtTelegrama;
+        private CheckBox chkUrgente;
     }
 }
